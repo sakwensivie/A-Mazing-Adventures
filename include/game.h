070dca4494a78d51018e4470b0363cf87d2bc13a
Samuel Akwensivie <sakwnesivie@gmail.com>
2023-03-20 11:02:12 +0000

    setting up the standard libraries

---
+++ include/game.h
@@ -0,0 +1,15 @@
+#ifndef GAME_H
+#define GAME_H
+
+/*Standard Libraries*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <math.h>
+#include <ctype.h>
+
+/*Third Party Libraries*/
+#include <SDL2/SDL.h>
+
+#endif
